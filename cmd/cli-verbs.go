61cff0964ccc6013d3e406043c55d7d7faafcad4
Sam McGeown <smcgeown@vmware.com>
2021-05-18 16:44:31 +0100

    Added basic shell completion

--- cmd/cli-verbs.go
+++ cmd/cli-verbs.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -94,6 +95,66 @@ var versionCmd = &cobra.Command{
 	},
 }
 
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
+
+Bash:
+
+  $ source <(cs-cli completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ cs-cli completion bash > /etc/bash_completion.d/cs-cli
+  # macOS:
+  $ cs-cli completion bash > /usr/local/etc/bash_completion.d/cs-cli
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ cs-cli completion zsh > "${fpath[1]}/_cs-cli"
+
+  # You will need to start a new shell for this setup to take effect.
+
+fish:
+
+  $ cs-cli completion fish | source
+
+  # To load completions for each session, execute once:
+  $ cs-cli completion fish > ~/.config/fish/completions/cs-cli.fish
+
+PowerShell:
+
+  PS> cs-cli completion powershell | Out-String | Invoke-Expression
+
+  # To load completions for every new session, run:
+  PS> cs-cli completion powershell > cs-cli.ps1
+  # and source this file from your PowerShell profile.
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(getCmd)
 	rootCmd.AddCommand(updateCmd)
@@ -101,4 +162,5 @@ func init() {
 	rootCmd.AddCommand(deleteCmd)
 	rootCmd.AddCommand(configCmd)
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(completionCmd)
 }
